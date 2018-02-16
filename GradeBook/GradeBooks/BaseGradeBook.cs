3d807d7d475b22d4cebbdfcdf2eff05857473cd9
Scott Tucker <stucker@leapagency.com>
2018-02-16 14:13:36 -0500

    q

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -109,32 +109,27 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+			var gpa = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    gpa = 4;
+					break;
                 case 'B':
-                    return 3;
+                    gpa = 3;
+					break;
                 case 'C':
-                    return 2;
+                    gpa = 2;
+					break;
                 case 'D':
-                    return 1;
+                    gpa = 1;
+					break;
             }
 			if (IsWeighted && (studentType == StudentType.DualEnrolled || studentType == StudentType.Honors))
 			{
-				switch (letterGrade)
-				{
-					case 'A':
-						return 5;
-					case 'B':
-						return 4;
-					case 'C':
-						return 3;
-					case 'D':
-						return 2;
-				}
+				gpa += 1;
 			}
-            return 0;
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
